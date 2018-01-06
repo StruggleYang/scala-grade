c82d70ed4ed2f092d620aed445f70a88d8c1f844
StruggleYang <yq1724555319@gmail.com>
2018-01-06 18:40:32 +0800

    ,

--- src/main/java/test/IdTest.java
+++ src/main/java/test/IdTest.java
@@ -6,29 +6,31 @@ import test.sequence.SnowflakeIdGenerator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class IdTest implements Runnable {
+public class IdTest{
     public static void main(String[] args) {
         // 创建指定最大线程数的线程池执行器
         ExecutorService executor = Executors.newFixedThreadPool(3);
         //创建任务并添加到线程池
         for(int i = 0 ;i<3;i++){
-            executor.execute(new IdTest());
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        SnowflakeIdGenerator sg =SnowflakeIdGenerator.class.newInstance();
+                        sg.init();
+                        for (int i = 0;i< 1000;i++){
+                            DbRep.addId(sg.next(),Thread.currentThread().getName());
+                            System.out.println(sg.next()+"----"+Thread.currentThread().getName());
+                        }
+                    }catch (Exception ex){
+                        ex.printStackTrace();
+                    }
+                }
+            });
         }
         // 关闭执行器
         executor.shutdown();
     }
 
-    @Override
-    public void run() {
-        try {
-            SnowflakeIdGenerator sg =SnowflakeIdGenerator.class.newInstance();
-            sg.init();
-            for (int i = 0;i< 10000;i++){
-                DbRep.addId(sg.next(),Thread.currentThread().getName());
-                System.out.println(sg.next()+"----"+Thread.currentThread().getName());
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-    }
+
 }
